11b391f9738b6676046fab49a5b42d1b8f6ac86c
Jeongdasilver <alal8272@gmail.com>
2019-03-19 15:07:51 +0900

    init

---
+++ EachOther/EachOther/VC/LoginVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EachOther
+//
+//  Created by daeun on 19/03/2019.
+//  Copyright © 2019 daeun. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
