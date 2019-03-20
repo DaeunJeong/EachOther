7b65c6a2773e816026ed59afac4ed8458da98aaa
Jeongdasilver <alal8272@gmail.com>
2019-03-20 16:01:32 +0900

    UPDATE UI
    upsate navigationcontroller ishidden
    button shape

--- EachOther/EachOther/VC/StartVC.swift
+++ EachOther/EachOther/VC/StartVC.swift
@@ -12,9 +12,10 @@ class StartVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.isNavigationBarHidden = true
+    }
 }
-
