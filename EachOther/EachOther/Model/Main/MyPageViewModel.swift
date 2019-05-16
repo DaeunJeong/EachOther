239515ec94b36a5d7c82db8d24831b18fc165f18
Jeongdasilver <alal8272@gmail.com>
2019-05-16 14:18:22 +0900

    ADD MyPage

---
+++ EachOther/EachOther/Model/Main/MyPageViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  MyPageViewModel.swift
+//  EachOther
+//
+//  Created by daeun on 16/05/2019.
+//  Copyright © 2019 daeun. All rights reserved.
+//
+
+import Foundation
---
+++ EachOther/EachOther/VC/Main/MyPageVC.swift
@@ -0,0 +1,17 @@
+//
+//  MyPageVC.swift
+//  EachOther
+//
+//  Created by daeun on 16/05/2019.
+//  Copyright © 2019 daeun. All rights reserved.
+//
+
+import UIKit
+
+class MyPageVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
