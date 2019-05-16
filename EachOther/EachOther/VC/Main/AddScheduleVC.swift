7f3344b10fdbd69b8b947a1a81c5658a9bb6c917
Jeongdasilver <alal8272@gmail.com>
2019-05-16 09:09:12 +0900

    UPDATE AddSchedule

--- EachOther/EachOther/VC/Main/AddScheduleVC.swift
+++ EachOther/EachOther/VC/Main/AddScheduleVC.swift
@@ -7,11 +7,36 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+
 
 class AddScheduleVC: UIViewController {
-
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var commentTextField: UITextField!
+    
+    var addScheduleViewModel: AddScheduleViewModel!
+    let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        addScheduleViewModel = AddScheduleViewModel()
+        let completeButton = UIBarButtonItem(title: "완료", style: .done, target: nil, action: nil)
+        self.navigationItem.rightBarButtonItem = completeButton
+        
+        let input = AddScheduleViewModel.Input(clickComplete: completeButton.rx.tap.asSignal(), comment: commentTextField.rx.text.orEmpty.asDriver(), date: datePicker.rx.date.asDriver())
+        
+        let output = addScheduleViewModel.transform(input: input)
+        
+        output.result.asObservable().subscribe { result in
+            if let result = result.element {
+                switch result {
+                case .success: print("SUCCESS")
+                case .fail: print("FAIL")
+                case .none: print("NONE")
+                }
+            }
+        }.disposed(by: disposeBag)
     }
 }
--- EachOther/EachOther/VC/Main/AddScheduleViewModel.swift
+++ EachOther/EachOther/VC/Main/AddScheduleViewModel.swift
@@ -7,3 +7,72 @@
 //
 
 import Foundation
+import RxCocoa
+import RxSwift
+import FirebaseFirestore
+
+class AddScheduleViewModel {
+    var db: Firestore!
+    let disposeBag = DisposeBag()
+    
+    init() {
+        let settings = FirestoreSettings()
+        Firestore.firestore().settings = settings
+        db = Firestore.firestore()
+    }
+    
+    struct Input {
+        let clickComplete: Signal<Void>
+        let comment: Driver<String>
+        let date: Driver<Date>
+    }
+    
+    struct Output {
+        let result: Driver<AddScheduleResult>
+    }
+
+    func transform(input: Input) -> Output {
+        
+        let familyCode: String = UserDefaults.standard.string(forKey:"FAMILYCODE") ?? ""
+        let result = PublishRelay<AddScheduleResult>()
+        let dateString = BehaviorRelay<String>(value: "")
+        let comment = BehaviorRelay<String>(value: "")
+        
+        input.date.asObservable().subscribe { date in
+            if let date = date.element {
+                let formatter = DateFormatter()
+                formatter.dateFormat = "yyyy MM dd"
+                dateString.accept(formatter.string(from: date))
+            } else {
+                dateString.accept("")
+            }
+        }.disposed(by: disposeBag)
+        
+        input.comment.asObservable().subscribe { inputComment in
+            if let inputComment = inputComment.element {
+                comment.accept(inputComment)
+            }
+            
+        }.disposed(by: disposeBag)
+        
+        input.clickComplete.asObservable().subscribe { [weak self] _ in
+            guard let strongSelf = self else {return}
+
+            strongSelf.db.collection(familyCode).document("schedule").setData(["date": dateString.value,"comment": comment.value]) { err in
+                if let err = err {
+                    dump(err)
+                    result.accept(.fail)
+                } else {
+                    result.accept(.success)
+                }
+            }
+        }.disposed(by: disposeBag)
+        
+        return Output(result: result.asDriver(onErrorJustReturn: .none))
+        
+    }
+}
+
+enum AddScheduleResult {
+    case none,success,fail
+}
